c6bbe063fddc5ecac5907b3d64c47cc794c70724
suresh200819 <79845886+suresh200819@users.noreply.github.com>
2023-07-25 19:25:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "google_compute_address" "public_ip" {
+  name    = var.public_ip_name
+  project = var.project
+  region  = var.region
+}
+
+resource "google_compute_instance" "gce_instance" {
+  name         = var.instance_name
+  project      = var.project
+  machine_type = var.machine_type
+  zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image    = var.image_name
+    }
+  }
+  network_interface {
+    network = "default"
+  access_config {
+    nat_ip = google_compute_address.public_ip.address
+    }
+  }
+  tags = ["web", "db"]
+}
